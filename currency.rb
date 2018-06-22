4a29ef4d2980f7196ac6466bb663cff9a2f162f8
Myakkatt <myao_03@icloud.com>
2018-06-22 17:21:24 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,17 @@
-# Write your cod here
+# Write your code here
+# i have to make a code that converts USD to Euros
+# i also have to make a code that will convet Euros to USD
+
+def usd_to_eur(usd)
+  #if you subtract the Euro from the USD you get a #. If you divided that number by the USD you get 0.14. 
+  
+  puts \
+end
+
+def eur_to_usd(euro)
+  
+  puts "8.59 euros = 10 usd"
+end
+  
+  
+  usd_to_eur()
